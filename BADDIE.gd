cc55d2d06e4d1af622909c70ea6e13662fcfacbd
Mangmasta <mangmasta@gmail.com>
2021-05-29 18:28:25 -0700

    Revert "Merge branch 'main' of https://github.com/TheMistersJam/gamejam01 into main"
    This reverts commit cd489012b2df6f2a08913758d05740f627607696, reversing
    changes made to 49e36882d2675aa41dc6d7016415312c61ff7535.

--- BADDIE.gd
+++ BADDIE.gd
@@ -11,6 +11,7 @@ const STATE_TIME = 1
 var speed = 0
 var tim
 var pouncePos = Vector2(0,0)
+var spawn_anim = "spawn_sky"
 
 onready var tw = get_node("Tween")
 onready var anim = get_node("AnimationPlayer")
@@ -19,64 +20,65 @@ onready var pl = get_tree().get_root().get_node("World/player")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tim = get_node("Timer")
-	tim.wait_time = STATE_TIME
-	tim.start()
+	
+	anim.play(spawn_anim)
 
 func on_hit(damage):
 	hp -= damage
+	$damageAnim.play("damaged")
 	if hp <= 0:
+		#probably do something fancier than just deleting itself
+		get_tree().get_root().get_node("World").guy_dead()
 		queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	move_and_slide((pouncePos - position)* speed *delta)
+	#move_and_slide((pouncePos - position)* speed *delta)
+	move_and_slide(pouncePos* speed *delta)
 
 
 func _on_Timer_timeout():
-	print(state)
+	#print(state)
 	match state:
 		0:
-			anim.play("idle")
-			state = 7
-		1:
-			#tw.interpolate_method(self, "move_and_slide", Vector2(0,0), Vector2(SPEED, 0), 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-			#tw.interpolate_property(self, "moveVec", Vector2(0, 0), Vector2(SPEED, 0), STATE_TIME, Tween.TRANS_QUART, Tween.EASE_IN_OUT)
-			tw.interpolate_property(self, "speed", 0, SPEED_MAX, STATE_TIME, Tween.TRANS_QUART, Tween.EASE_IN_OUT)
-			pouncePos = pl.position
-			tw.start()
-			tim.stop()
-			anim.play("pounce", -1, 0.7)
-			state = 2
-			
-		3:
-			anim.play("idle")
-			state = 6
-		4:
-			tw.interpolate_property(self, "speed", 0, -SPEED_MAX, STATE_TIME, Tween.TRANS_QUART, Tween.EASE_IN_OUT)
-			pouncePos = pl.position
-			tw.start()
-			tim.stop()
-			anim.play("pounce", -1, 0.7)
-			state = 5
-		6:
-			anim.play("shake")
-			state = 4
-		7:
-			anim.play("shake")
+			anim.play("idle") #maybe should be walking
+			pouncePos = Vector2((randf()*2)-1, (randf()*2)-1)
+			pouncePos = pouncePos.normalized()
+			speed = 5000
 			state = 1
+			tim.set_wait_time((randf()*1) + 1)
+			tim.start()
+		1:
+			speed = 0
+			anim.play("shake")
+			state = 2
+		2:
+			#tw.interpolate_property(self, "speed", 0, SPEED_MAX, STATE_TIME, Tween.TRANS_QUART, Tween.EASE_IN_OUT)
+			speed = 30000
+			pouncePos = pl.position - position
+			pouncePos = pouncePos.normalized()
+			#tw.start()
+			#tim.stop()
+			anim.play("pounce") #, -1, 0.7)
+			tim.set_wait_time(1)
+			tim.start()
+			state = 3
+		3:
+			speed = 0
+			anim.play("idle")
+			tim.set_wait_time(0.5)
+			tim.start()
+			state = 0
 
 
-func _on_Tween_tween_completed(object, key):
-	if object == self and key == ":speed":
-		match state:
-			2:
-				tw.interpolate_property(self, "speed", speed, 0, STATE_TIME, Tween.TRANS_QUART, Tween.EASE_IN_OUT)
-				tw.start()
-				state = 0 #used to be 3, but we dont move back now
-				tim.start()
-			5:
-				tw.interpolate_property(self, "speed", speed, 0, STATE_TIME, Tween.TRANS_QUART, Tween.EASE_IN_OUT)
-				tw.start()
-				state = 0
-				tim.start()
-				
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if "spawn" in anim_name:
+		var t = Timer.new()
+		t.set_wait_time(randf())
+		t.set_one_shot(true)
+		self.add_child(t)
+		t.start()
+		yield(t, "timeout")
+		t.queue_free()
+		tim.wait_time = STATE_TIME
+		tim.start()
--- level1.gd
+++ level1.gd
@@ -1,18 +1,78 @@
 extends Node2D
 
 
-const NUM_ENEMIES = 3
+const NUM_ENEMIES = 10
+const NUM_ENEMIES_MAX_IN_ROOM = 3
+var enemiesleft = NUM_ENEMIES
+var bad_inst
+var player
+
+onready var windowTimer = get_node("windowTimer")
+var windowOcc = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	var bad_inst = preload("res://BADDIE.tscn")
-	for i in range(NUM_ENEMIES):
-		var bad = bad_inst.instance()
-		bad.position = Vector2((randi() % 1200) + 10, (randi() % 600) + 10)
-		add_child(bad)
+	bad_inst = preload("res://BADDIE.tscn")
+	player = get_node("player")
+	
+	var tospawn
+	if NUM_ENEMIES > NUM_ENEMIES_MAX_IN_ROOM:
+		tospawn = NUM_ENEMIES_MAX_IN_ROOM
+	else:
+		tospawn = NUM_ENEMIES
+	
+	enemiesleft -= tospawn
+	
+	for i in range(tospawn):
+		spawn_badguy()
 
+func guy_dead():
+	enemiesleft -= 1
+	if enemiesleft > 0:
+		spawn_badguy()
+
+func spawn_badguy():
+	var bad = bad_inst.instance()
+	var state = -1
+	while state < 0:
+		state = randi() % 2
+		print(state)
+		match state:
+			1:
+				if windowTimer.is_stopped() == true and windowOcc == false:
+					print("Spawning window?")
+					windowTimer.start()
+					windowOcc = true
+				else:
+					print("Cant spawn in window")
+					state = -1
+			0:
+				state = 0
+	match state:
+		0:
+			bad.position = Vector2((randi() % 970) + 80, (randi() % 240) + 300)
+			while abs((bad.position - player.position).length()) < 100:
+				bad.position = Vector2((randi() % 970) + 80, (randi() % 240) + 300)
+			bad.spawn_anim = "spawn_sky"
+		1:
+			#warning: mega jank inbound
+			var lengths = [Vector2(850, 600), Vector2(515 ,600), Vector2(200 ,600)]
+			var candidate = lengths[0]
+			if (candidate - player.position).length() < (lengths[1] - player.position).length():
+				candidate = lengths[1]
+			if (candidate - player.position).length() < (lengths[2] - player.position).length():
+				candidate = lengths[2]
+			bad.position = candidate
+			bad.spawn_anim = "spawn_window_bottom"
+			
+			
+	add_child(bad)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_windowTimer_timeout():
+	windowOcc = false
